27b5583f89d5f0b12cd5cf3d3385a8df18961cbc
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-01-06 11:15:34 -0500

    badge status done

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import greenBanner from "../assets/banners/green-banner.svg";
+import pic from "../assets/images/me.jpg";
 
+import "./styles/Badge.css";
 class Badge extends React.Component {
   render() {
     return (
-      <article>
-        <header>
-            <img src="" alt="header-banner"/>
+      <article className="badge">
+        <header className="badge_header">
+          <img src={greenBanner} alt="header-banner" />
+          <div className="header_info">
+            <h1>MATEHUS BARONASISIMOAS</h1>
+            <h5>TERRICOLA</h5>
+          </div>
         </header>
         <section className="profile">
-          <img src="" alt="user-photo" />
-          <h1>
-            Math <br /> Montanez
-          </h1>
-        </section>
-        <section className="description">
+          <div className="profile_photo">
+            <img src={pic} alt="user-profile" />
+          </div>
+          <div className="profile_description">
             <h4>Nationality: Terricola</h4>
-            <p>Hombre comun estatura promedio</p>
+            <p>Hombre comun, estatura promedio que come mucho y hace ejercicio</p>
             <small>Status: Alive</small>
+          </div>
         </section>
         <footer>
-            <h5>Peace and love</h5>
+          <h5>"Peace and love"</h5>
         </footer>
       </article>
     );
