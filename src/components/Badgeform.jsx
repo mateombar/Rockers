bc3a537b7097cd3a5b7c79d061ed47867441a5c4
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-01-07 10:42:49 -0500

    Badge form styles and first views

--- src/components/Badgeform.jsx
+++ src/components/Badgeform.jsx
@@ -1,16 +1,59 @@
 import React from "react";
-import './styles/Badgeform.css';
+import "./styles/Badgeform.css";
 class Badgeform extends React.Component {
+  handleChange = (e) => {
+    console.log({
+      name: e.target.name,
+      value: e.target.value,
+    });
+  };
+  handleClick = (e) => {
+    console.log("clicked");
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
   render() {
     return (
       <div className="badgeform">
         <h1>New Rocker</h1>
-        <form>
-            <div className="form-group">
-                <label >First Name</label>
-                <input type="text" name="firstName"/>
-            </div>
-            <button className="form-button">Save</button>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>First Name</label>
+            <input className="form-group__input" onChange={this.handleChange} type="text" name="firstName" />
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+            <input className="form-group__input" onChange={this.handleChange} type="text" name="lastName" />
+          </div>
+          <div className="form-group">
+            <label>Origin</label>
+            <select className="form-group__input" name="origin">
+              <option value="Earth">Earth</option>
+              <option value="Mars">Mars</option>
+              <option value="Juno">Juno</option>
+              <option value="Your mom">Your mom</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description</label>
+            <input className="form-group__input" onChange={this.handleChange} type="text" name="description" />
+          </div>
+          <div className="form-group">
+            <label>Status</label>
+            <select className="form-group__input" name="status">
+              <option value="Earth">Alive</option>
+              <option value="Mars">Death</option>
+              <option value="Juno">Unknown</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Quote</label>
+            <input className="form-group__input" onChange={this.handleChange} type="text" name="quote" />
+          </div>
+          <button onClick={this.handleClick} className="form-button">
+            Save
+          </button>
         </form>
       </div>
     );
