7ce9864784d92ce4538903f0577d91880af2a7e8
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-01-21 10:25:27 -0500

    Passing RockerBadge from a class to a function

--- src/components/RockerBadge.jsx
+++ src/components/RockerBadge.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import "./styles/RockerBadge.css";
-class RockerBadge extends React.Component {
-  render() {
-    const data = this.props.data;
-    return (
-      <article className="rocker_badge">
-        <div className="rocker_avatar">
-          <img src={data.avatarUrl} alt={`${data.firstName} avatar`} />
-        </div>
-        <div className="rocker_info">
-          <h3>
-            {data.firstName} {data.lastName}
-          </h3>
-          <p>{data.email}</p>
-          <p>{data.jobTitle}</p>
-          <pre>{data.status}</pre>
-        </div>
-      </article>
-    );
-  }
+function RockerBadge(props) {
+  const data = props.data;
+  return (
+    <article className="rocker_badge">
+      <div className="rocker_avatar">
+        <img src={data.avatarUrl} alt={`${data.firstName} avatar`} />
+      </div>
+      <div className="rocker_info">
+        <h3>
+          {data.firstName} {data.lastName}
+        </h3>
+        <p>{data.email}</p>
+        <p>{data.jobTitle}</p>
+        <pre>{data.status}</pre>
+      </div>
+    </article>
+  );
 }
 
 export default RockerBadge;
