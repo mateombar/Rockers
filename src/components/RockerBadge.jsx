f36cff3fce478f67f5c97de23cb8432ea4894d3d
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-01-27 09:30:56 -0500

    Changing paragraph character limit with css

--- src/components/RockerBadge.jsx
+++ src/components/RockerBadge.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./styles/RockerBadge.css";
 function RockerBadge(props) {
   const data = props.data;
-  const [email, setEmail] = useState(data.email);
-  useEffect(() => {
-    setEmail(`${email.substring(0, 12)}...`);
-  }, [email]);
   return (
     <article className="rocker_badge">
       <div className="rocker_avatar">
@@ -15,7 +11,7 @@ function RockerBadge(props) {
         <h3>
           {data.firstName} {data.lastName}
         </h3>
-        <p>{email}</p>
+        <p>{data.email}</p>
         <p>{data.jobTitle}</p>
         <pre>{data.status}</pre>
       </div>
